efe04342e616de19cb6a42f12d6facae19427aef
Andrea Parodi <a.parodi@inet2.it>
2015-08-10 16:19:05 +0200

    root level functions - primiteves only support

--- es6/index.js
+++ es6/index.js
@@ -1,3 +1,102 @@
-export default async function babelPluginKeepFlow() {
-  return Promise.resolve(42);
+
+// import { transform } from 'babel';
+
+function valueToMetaAssignment(name, meta) {
+  return {
+    type: 'ExpressionStatement',
+    expression: {
+      type: 'AssignmentExpression',
+      operator: '=',
+      left: {
+          type: 'MemberExpression',
+          computed: false,
+          object: {
+              type: 'Identifier',
+              name
+          },
+          property: {
+              type: 'Identifier',
+              name: '__meta'
+          }
+      },
+      right: meta
+    }
+  };
+}
+
+function mkTypeVisitor(resultType) {
+  return {
+    exit: node => {
+      node.__metaArg.type = resultType;
+    }
+  };
+}
+
+export default function babelPluginKeepFlow({ Plugin, types: t }) {
+  return new Plugin('babel-plugin-keepflow', {
+    visitor: {
+      AnyTypeAnnotation: mkTypeVisitor('any'),
+      StringTypeAnnotation: mkTypeVisitor('string'),
+      NumberTypeAnnotation: mkTypeVisitor('number'),
+      BooleanTypeAnnotation: mkTypeVisitor('boolean'),
+      VoidTypeAnnotation: mkTypeVisitor('void'),
+      MixedTypeAnnotation: mkTypeVisitor('mixed'),
+
+      FunctionDeclaration: {
+        enter(node) {
+          node.__meta = node.__meta || {
+            arguments: [],
+            returnType: {
+              type: 'any'
+            }
+          };
+
+          if (node.returnType) {
+            node.returnType.typeAnnotation.__metaArg = node.__meta.returnType;
+          }
+
+          node.params.forEach( p => {
+            const metaArg = {
+              name: p.name,
+              type: 'any'
+            };
+
+            node.__meta.arguments.push(metaArg);
+
+            if (p.typeAnnotation) {
+              p.typeAnnotation.typeAnnotation.__metaArg = metaArg;
+            }
+          });
+        },
+        exit(node, parent) {
+          if (parent.type !== 'ExportDefaultDeclaration' && parent.type !== 'ExportNamedDeclaration') {
+            return [
+              node,
+              valueToMetaAssignment(node.id.name, t.valueToNode(node.__meta))
+            ];
+          }
+        }
+      },
+      ExportNamedDeclaration: {
+        exit(node) {
+          if (node.declaration.type === 'FunctionDeclaration') {
+            return [
+              node,
+              valueToMetaAssignment(node.declaration.id.name, t.valueToNode(node.declaration.__meta))
+            ];
+          }
+        }
+      },
+      ExportDefaultDeclaration: {
+        exit(node) {
+          if (node.declaration.type === 'FunctionDeclaration') {
+            return [
+              node,
+              valueToMetaAssignment(node.declaration.id.name, t.valueToNode(node.declaration.__meta))
+            ];
+          }
+        }
+      }
+    }
+  });
 }
--- test/index_test.js
+++ test/index_test.js
@@ -3,12 +3,171 @@ if (process.env.TEST_RELEASE) {
   moduleRoot = '../dist';
 }
 
+import { transform } from 'babel';
+
 const babelPluginKeepFlow = require(moduleRoot);
+const Module = module.constructor;
+
+function requireFromString(src, filename) {
+  const m = new Module();
+  m.paths = module.paths;
+  m._compile(src, filename);
+  return m.exports;
+}
+
+const check = (source, expectedMeta, namedExport = null) => () => {
+  const result = transform(source, {
+    plugins: babelPluginKeepFlow
+  });
+
+  let test = requireFromString(result.code, __filename);
+  if (namedExport) {
+    test = test[namedExport];
+  }
+
+  test.__meta.should.be.deep.equal(expectedMeta);
+};
 
 describe('babelPluginKeepFlow', () => {
-  it('works', async () => {
-    const result = await babelPluginKeepFlow();
-    result.should.be.equal(42);
-  });
+  it('support string type as argument', check(
+    'function testFun(a: string) {}\n export default testFun;',
+    {
+      returnType: {
+        type: 'any'
+      },
+      arguments: [{
+        name: 'a',
+        type: 'string'
+      }]
+    }
+  ));
+
+  it('support boolean type as argument', check(
+    'function testFun(a: boolean) {}\n export default testFun;',
+    {
+      returnType: {
+        type: 'any'
+      },
+      arguments: [{
+        name: 'a',
+        type: 'boolean'
+      }]
+    }
+  ));
+
+  it('support number type as argument', check(
+    'function testFun(a: number) {}\n export default testFun;',
+    {
+      returnType: {
+        type: 'any'
+      },
+      arguments: [{
+        name: 'a',
+        type: 'number'
+      }]
+    }
+  ));
+
+  it('support any type as argument', check(
+    'function testFun(a: any) {}\n export default testFun;',
+    {
+      returnType: {
+        type: 'any'
+      },
+      arguments: [{
+        name: 'a',
+        type: 'any'
+      }]
+    }
+  ));
+
+  it('support void type as argument', check(
+    'function testFun(a: void) {}\n export default testFun;',
+    {
+      returnType: {
+        type: 'any'
+      },
+      arguments: [{
+        name: 'a',
+        type: 'void'
+      }]
+    }
+  ));
+
+  it('support mixed type as argument', check(
+    'function testFun(a: mixed) {}\n export default testFun;',
+    {
+      returnType: {
+        type: 'any'
+      },
+      arguments: [{
+        name: 'a',
+        type: 'mixed'
+      }]
+    }
+  ));
+
+  it('support multiple typed arguments', check(
+    'function testFun(a: string,b: boolean) {}\n export default testFun;',
+    {
+      returnType: {
+        type: 'any'
+      },
+      arguments: [{
+        name: 'a',
+        type: 'string'
+      }, {
+        name: 'b',
+        type: 'boolean'
+      }]
+    }
+  ));
+
+  it('evaluate untyped as any', check(
+    'function testFun(a,b) {}\n export default testFun;',
+    {
+      returnType: {
+        type: 'any'
+      },
+      arguments: [{
+        name: 'a',
+        type: 'any'
+      }, {
+        name: 'b',
+        type: 'any'
+      }]
+    }
+  ));
+
+  it('support function return types', check(
+    'function testFun(): boolean {}\n export default testFun;',
+    {
+      arguments: [],
+      returnType: {
+        type: 'boolean'
+      }
+    }
+  ));
+
+  it('support function export syntax', check(
+    'export default function testFun(): boolean {}',
+    {
+      arguments: [],
+      returnType: {
+        type: 'boolean'
+      }
+    }
+  ));
+
+  it('support non-default function export syntax', check(
+    'export function testFun(): boolean {}',
+    {
+      arguments: [],
+      returnType: {
+        type: 'boolean'
+      }
+    },
+    'testFun'
+  ));
 });
 
